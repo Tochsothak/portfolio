8eae4055dce1ba4a7e9a70ee67c67f4574a3f673
Tochsothak <tochsothak882@gmail.com>
2024-09-03 22:38:54 +0700

    add scroll function main section and social icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,23 @@
 /*navigation Bar Function*/
 function myMenuFunction() {
-  var menuBtn = document.getElementById("myNavMenu");
-  if (menuBtn.className === "nav-menu") {
-    menuBtn.className += "responsive";
-  } else {
-    menuBtn.className = "nav-menu";
-  }
+    var menuBtn = document.getElementById("myNavMenu");
+    if (menuBtn.className === "nav-menu") {
+        menuBtn.className += " responsive";
+    } else {
+        menuBtn.className = "nav-menu";
+    }
 }
+
+/*--Add shadow on navigation bar while scrolling--*/
+window.onscroll = function() {
+    const navHeader = document.getElementById("header");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navHeader.style.boxShadow = "0 1px 6px rgba(0,0,0,0.1)";
+        navHeader.style.height = "70";
+        navHeader.style.lineHeight = "70px";
+    } else {
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+    }
+};
