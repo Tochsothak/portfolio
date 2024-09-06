06493120b0fd6c090bb944e71c32df8b8aa80844
Tochsothak <tochsothak882@gmail.com>
2024-09-06 22:38:26 +0700

    done with contact info and footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,29 @@ function headerShadow() {
         navHeader.style.lineHeight = "90px";
     }
 }
+/*--Typing Effect--*/
+var typingEffect = new Typed(".typedText", {
+    strings: ["a Software Developmet Student."],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000,
+});
+
+/*Social Reveal Animations*/
+const sr = ScrollReaveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+});
+/*home*/
+sr.reveal(".featured-text-cart", {});
+sr.reveal(".featured-name", { delay: 100 });
+sr.reveal(".featured-text-info", { delay: 200 });
+sr.reveal(".featured-text-btn", { delay: 200 });
+sr.reveal(".social_icon", { delay: 200 });
+sr.reveal(".featured-image", { delay: 300 });
+
+/*--Project Box--*/
+sr.reveal(".project-box", { interval: 200 });
