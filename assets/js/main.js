dcf2b1c670fcbcaa2afe640fde4a637aceb4bff7
Tochsothak <tochsothak882@gmail.com>
2024-09-07 18:54:25 +0700

    Add Scroll Reveal to Home Section About Section and Skills Box

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,7 @@ function headerShadow() {
 }
 /*--Typing Effect--*/
 var typingEffect = new Typed(".typedText", {
-    strings: ["a Software Developmet Student."],
+    strings: ["a Software Developmet Student"],
     loop: true,
     typeSpeed: 100,
     backSpeed: 80,
@@ -35,10 +35,9 @@ var typingEffect = new Typed(".typedText", {
 });
 
 /*Social Reveal Animations*/
-const sr = ScrollReaveal({
+const sr = ScrollReveal({
     origin: "top",
     distance: "80px",
-    duration: 2000,
     reset: true,
 });
 /*home*/
@@ -51,3 +50,32 @@ sr.reveal(".featured-image", { delay: 300 });
 
 /*--Project Box--*/
 sr.reveal(".project-box", { interval: 200 });
+
+/*--Heading--*/
+sr.reveal(".top-header", {});
+
+/*--About info And  Contact info--*/
+const srLeft = ScrollReveal({
+    origin: "left",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+});
+srLeft.reveal(".about-info", { delay: 100 });
+srLeft.reveal(".contact-info", { delay: 100 });
+
+/*--About skill And Form Box--*/
+const srRight = ScrollReveal({
+    origin: "rightt",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+});
+srRight.reveal(".skills-box", { delay: 100 });
+srRight.reveal(".form-control", { delay: 100 });
+
+const section = document.querySelector("section[id]");
+
+function scrollY() {
+    scrollY = window.scrollY;
+}
